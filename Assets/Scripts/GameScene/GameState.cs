6b364bec3873ece2fc8ccc2d7adb444a568b0569
Fırat Tamur <ftamur16@ku.edu.tr>
2022-04-28 01:59:26 +0300

    feat: add ui elements and controllers for start screen

---
+++ Assets/Scripts/GameScene/GameState.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public enum GameState
+{
+    
+    MENU,
+    PLAY,
+    LOSE
+
+}
--- Assets/Scripts/GameScene/PlayerController.cs
+++ Assets/Scripts/GameScene/PlayerController.cs
@@ -10,10 +10,12 @@ public class PlayerController : MonoBehaviour
     public float speed = 10.0f;    
     public float moveInput;
 
+    [SerializeField] GameController gameController;
+
     // Start is called before the first frame update
     void Start()
     {
-        rb.velocity = new Vector3(0, 0, speed);
+    
     }
 
     // Update is called once per frame
@@ -26,8 +28,9 @@ public class PlayerController : MonoBehaviour
 
     void FixedUpdate()
     {
-
-        rb.velocity = new Vector3(moveInput * speed, rb.velocity.y, speed);
+        
+        if (gameController.GetState() == GameState.PLAY)
+            rb.velocity = new Vector3(moveInput * speed, rb.velocity.y, speed);
 
     }
 
